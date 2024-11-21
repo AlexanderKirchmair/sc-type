4981398f151a6d184404f6d72149f540c7377cf0
kirchmair <alexander.kirchmair@i-med.ac.at>
2024-11-21 17:45:01 +0100

    Simplify sourcing

--- R/sctype_wrapper.R
+++ R/sctype_wrapper.R
@@ -1,24 +1,4 @@
-#' @title sctype source files
-#' @name sctype_source
-#' @description loads sctype functions needed for an automated cell type annotation . 
-#' @details none
-#' @param none 
-#' @return original ScType database
-#' @export
-#' @examples
-#' db_=sctype_source()
-#' 
-sctype_source <- function(){
-    # load tissue auto detect
-    source("./R/auto_detect_tissue_type.R")
-    # load gene set preparation function
-    source("./R/gene_sets_prepare.R")
-    # load cell type annotation function
-    source("./R/sctype_score_.R")
-    # load ScType database
-    db_ = "./ScTypeDB_full.xlsx"
-    return(db_)
-}
+
 
 #' @title Run sctype analysis on Seurat object
 #' @name run_scType
@@ -39,10 +19,7 @@ sctype_source <- function(){
 #' 
 #' @export
 #' 
-
-
-run_sctype <- function(seurat_object, known_tissue_type = NULL, assay = "RNA", scaled = TRUE, custom_marker_file = NULL, plot = FALSE, name = "sctype_classification") {
-    db_=sctype_source()
+run_sctype <- function(seurat_object, known_tissue_type = NULL, assay = "RNA", scaled = TRUE, custom_marker_file = NULL, plot = FALSE, dp="ScTypeDB_full.xlsx", name = "sctype_classification",  species="human") {
     # Check for missing arguments
     if (is.null(seurat_object)) {
         stop("Argument 'seurat_object' is missing")
@@ -67,7 +44,7 @@ run_sctype <- function(seurat_object, known_tissue_type = NULL, assay = "RNA", s
     }
     
     # Prepare gene sets
-    gs_list = gene_sets_prepare(custom_marker_file, tissue_type)
+    gs_list = gene_sets_prepare(custom_marker_file, tissue_type, species=species)
 
     data_type <- if (scaled) "scale.data" else "counts"  
     package_type <- data_type %in% names(attributes(seurat_object[[assay]]))
@@ -77,7 +54,7 @@ run_sctype <- function(seurat_object, known_tissue_type = NULL, assay = "RNA", s
         
         print("Using Seurat v4 object")
         es.max = sctype_score(scRNAseqData = slot(seurat_object[[assay]], data_type),
-                              scaled = TRUE,gs = gs_list$gs_positive, 
+                              scaled = TRUE,gs = gs_list$gs_positive,
                               gs2 = gs_list$gs_negative)   
         
     } else{
@@ -117,3 +94,187 @@ run_sctype <- function(seurat_object, known_tissue_type = NULL, assay = "RNA", s
     print(text_)
     return(seurat_object_res)
 }    
+
+
+
+# GNU General Public License v3.0 (https://github.com/IanevskiAleksandr/sc-type/blob/master/LICENSE)
+# Written by Aleksandr Ianevski <aleksandr.ianevski@helsinki.fi>, June 2021
+#
+# Functions on this page:
+# auto_detect_tissue_type: automatically detect a tissue type of the dataset
+#
+# @params: path_to_db_file - DB file with cell types
+# @params: seuratObject - The Seurat Object from wich to extract the input scRNA-seq matrix (rownames - genes, column names - cells), 
+# @params: scale - indicates whether the matrix is scaled (TRUE by default)
+# @params: assay - e.g. RNA, SCT, integrated
+auto_detect_tissue_type <- function(path_to_db_file, seuratObject, scaled, assay = "RNA", ...){
+    
+    # get all tissue types in DB
+    db_read = openxlsx::read.xlsx(path_to_db_file); tissues_ = unique(db_read$tissueType); result_ = c()
+    
+    for(tissue in tissues_){ print(paste0("Checking...", tissue));
+        
+        # prepare gene sets
+        gs_list = gene_sets_prepare(path_to_db_file, tissue);
+
+        # check Seurat version
+        package_type <- substr(packageVersion("Seurat"), 1, 1)
+        data_type <- if (scaled) "scale.data" else "counts"
+        obj <- if (package_type == "5") {
+          as.matrix(seuratObject[[assay]]$data_type)
+        } else {
+          as.matrix(seuratObject[[assay]]@data_type)
+        }
+        
+        es.max = sctype_score(scRNAseqData = obj, scaled = scaled, gs = gs_list$gs_positive, gs2 = gs_list$gs_negative, 
+                              marker_sensitivity = gs_list$marker_sensitivity, verbose=!0);
+        
+        cL_resutls = do.call("rbind", lapply(unique(seuratObject@meta.data$seurat_clusters), function(cl){
+            
+            es.max.cl = sort(rowSums(es.max[ ,rownames(seuratObject@meta.data[seuratObject@meta.data$seurat_clusters==cl, ])]), decreasing = !0)
+            head(data.frame(cluster = cl, type = names(es.max.cl), scores = es.max.cl), 10)
+        }))
+        
+        dt_out = cL_resutls %>% group_by(cluster) %>% top_n(n = 1)
+        
+        # return mean score for tissue
+        result_ = rbind(result_, data.frame(tissue = tissue, score = mean(dt_out$scores)))
+    }
+    
+    # order by mean score
+    result_ = result_[order(-result_$score),]
+    
+    # plot 
+    barplot(height=result_$score, names=result_$tissue, col=rgb(0.8,0.1,0.1,0.6),
+            xlab="Tissue", ylab="Summary score",  main="The higher summary score, the more likely tissue type is")
+    
+    result_
+}
+
+
+# GNU General Public License v3.0 (https://github.com/IanevskiAleksandr/sc-type/blob/master/LICENSE)
+# Written by Aleksandr Ianevski <aleksandr.ianevski@helsinki.fi>, June 2021
+#
+# Functions on this page:
+# gene_sets_prepare: prepare gene sets and calculate marker sensitivity from input Cell Type excel file
+#
+# @params: path_to_db_file - DB file with cell types
+# @cell_type - cell type (e.g. Immune system, Liver, Pancreas, Kidney, Eye, Brain)
+#
+gene_sets_prepare <- function(path_to_db_file, tissueType=NULL, species="human"){
+  
+  cell_markers = openxlsx::read.xlsx(path_to_db_file)
+  if (!is.null(tissueType)){
+    cell_markers = cell_markers[cell_markers$tissueType == tissueType,] 
+  }
+  cell_markers$geneSymbolmore1 = gsub(" ","",cell_markers$geneSymbolmore1); cell_markers$geneSymbolmore2 = gsub(" ","",cell_markers$geneSymbolmore2)
+  
+  # correct gene symbols from the given DB (up-genes)
+  cell_markers$geneSymbolmore1 = sapply(1:nrow(cell_markers), function(i){
+    
+    markers_all = gsub(" ", "", unlist(strsplit(cell_markers$geneSymbolmore1[i],",")))
+    markers_all = markers_all[markers_all != "NA" & markers_all != ""]
+    markers_all = sort(markers_all)
+    
+    if(length(markers_all) > 0){
+      suppressMessages({markers_all = unique(na.omit(checkGeneSymbols(markers_all, species = species)$Suggested.Symbol))})
+      paste0(markers_all, collapse=",")
+    } else {
+      ""
+    }
+  })
+  
+  # correct gene symbols from the given DB (down-genes)
+  cell_markers$geneSymbolmore2 = sapply(1:nrow(cell_markers), function(i){
+    
+    markers_all = gsub(" ", "", unlist(strsplit(cell_markers$geneSymbolmore2[i],",")))
+    markers_all = markers_all[markers_all != "NA" & markers_all != ""]
+    markers_all = sort(markers_all)
+    
+    if(length(markers_all) > 0){
+      suppressMessages({markers_all = unique(na.omit(checkGeneSymbols(markers_all, species = species)$Suggested.Symbol))})
+      paste0(markers_all, collapse=",")
+    } else {
+      ""
+    }
+  })
+  
+  cell_markers$geneSymbolmore1 = gsub("///",",",cell_markers$geneSymbolmore1);cell_markers$geneSymbolmore1 = gsub(" ","",cell_markers$geneSymbolmore1)
+  cell_markers$geneSymbolmore2 = gsub("///",",",cell_markers$geneSymbolmore2);cell_markers$geneSymbolmore2 = gsub(" ","",cell_markers$geneSymbolmore2)
+   
+  gs = lapply(1:nrow(cell_markers), function(j) gsub(" ","",unlist(strsplit(toString(cell_markers$geneSymbolmore1[j]),",")))); names(gs) = cell_markers$cellName
+  gs2 = lapply(1:nrow(cell_markers), function(j) gsub(" ","",unlist(strsplit(toString(cell_markers$geneSymbolmore2[j]),",")))); names(gs2) = cell_markers$cellName
+  
+  list(gs_positive = gs, gs_negative = gs2)
+}
+
+
+# GNU General Public License v3.0 (https://github.com/IanevskiAleksandr/sc-type/blob/master/LICENSE)
+# Written by Aleksandr Ianevski <aleksandr.ianevski@helsinki.fi>, June 2021
+#
+# Functions on this page:
+# sctype_score: calculate ScType scores and assign cell types
+#
+# @params: scRNAseqData - input scRNA-seq matrix (rownames - genes, column names - cells), 
+# @params: scale - indicates whether the matrix is scaled (TRUE by default)
+# @params: gs - list of gene sets positively expressed in the cell type 
+# @params: gs2 - list of gene sets that should not be expressed in the cell type (NULL if not applicable)
+sctype_score <- function(scRNAseqData, scaled = !0, gs, gs2 = NULL, gene_names_to_uppercase = FALSE, ...){
+  
+  # check input matrix
+  if(!is.matrix(scRNAseqData)){
+    warning("scRNAseqData doesn't seem to be a matrix")
+  } else {
+    if(sum(dim(scRNAseqData))==0){
+       warning("The dimension of input scRNAseqData matrix equals to 0, is it an empty matrix?")
+    }
+  }
+  
+  # marker sensitivity
+  marker_stat = sort(table(unlist(gs)), decreasing = T); 
+  marker_sensitivity = data.frame(score_marker_sensitivity = scales::rescale(as.numeric(marker_stat), to = c(0,1), from = c(length(gs),1)),
+                                      gene_ = names(marker_stat), stringsAsFactors = !1)
+
+  # convert gene names to Uppercase
+  if(gene_names_to_uppercase){
+    rownames(scRNAseqData) = toupper(rownames(scRNAseqData));
+  }
+  
+  # subselect genes only found in data
+  names_gs_cp = names(gs); names_gs_2_cp = names(gs2);
+  gs = lapply(1:length(gs), function(d_){ 
+    GeneIndToKeep = rownames(scRNAseqData) %in% as.character(gs[[d_]]); rownames(scRNAseqData)[GeneIndToKeep]})
+  gs2 = lapply(1:length(gs2), function(d_){ 
+    GeneIndToKeep = rownames(scRNAseqData) %in% as.character(gs2[[d_]]); rownames(scRNAseqData)[GeneIndToKeep]})
+  names(gs) = names_gs_cp; names(gs2) = names_gs_2_cp;
+  cell_markers_genes_score = marker_sensitivity[marker_sensitivity$gene_ %in% unique(unlist(gs)),]
+  
+  # z-scale if not
+  if(!scaled) Z <- t(scale(t(scRNAseqData))) else Z <- scRNAseqData
+  
+  # multiple by marker sensitivity
+  for(jj in 1:nrow(cell_markers_genes_score)){
+    Z[cell_markers_genes_score[jj,"gene_"], ] = Z[cell_markers_genes_score[jj,"gene_"], ] * cell_markers_genes_score[jj, "score_marker_sensitivity"]
+  }
+  
+  # subselect only with marker genes
+  Z = Z[unique(c(unlist(gs),unlist(gs2))), ]
+  
+  # combine scores
+  es = do.call("rbind", lapply(names(gs), function(gss_){ 
+    sapply(1:ncol(Z), function(j) {
+      gs_z = Z[gs[[gss_]], j]; gz_2 = Z[gs2[[gss_]], j] * -1
+      sum_t1 = (sum(gs_z) / sqrt(length(gs_z))); sum_t2 = sum(gz_2) / sqrt(length(gz_2));
+      if(is.na(sum_t2)){
+        sum_t2 = 0;
+      }
+      sum_t1 + sum_t2
+    })
+  })) 
+  
+  dimnames(es) = list(names(gs), colnames(Z))
+  es.max <- es[!apply(is.na(es) | es == "", 1, all),] # remove na rows
+ 
+  es.max
+}
+
